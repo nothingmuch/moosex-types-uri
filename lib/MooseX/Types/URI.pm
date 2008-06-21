5daad7490400f1f548a6460ce3369be12ccaa073
Yuval Kogman <nothingmuch@woobling.org>
2008-06-21 14:08:42 +0000

    foo

--- lib/MooseX/Types/URI.pm
+++ lib/MooseX/Types/URI.pm
@@ -21,21 +21,30 @@ use namespace::clean;
 
 use MooseX::Types -declare => [qw(Uri _UriWithBase _Uri FileUri DataUri)];
 
-my $uri = register_type_constraint Uri(), Moose::Meta::TypeConstraint::Union->new(   
-    type_constraints => [
-        class_type( _Uri,         { class => "URI" } ),
-        class_type( _UriWithBase, { class => "URI::WithBase" } ),
-    ],
-    optimize_as => sub { blessed($_[0]) && $_[0]->isa("URI") || $_[0]->isa("URI::WithBase") },
+my $uri = Moose::Meta::TypeConstraint->new(
+    name   => Uri,
+    parent => Moose::Meta::TypeConstraint::Union->new(
+        name => join("|", _Uri, _UriWithBase),
+        type_constraints => [
+            class_type( _Uri,         { class => "URI" } ),
+            class_type( _UriWithBase, { class => "URI::WithBase" } ),
+        ],
+    ),
+    optimized => sub {
+        local $@;
+        blessed($_[0]) && ( $_[0]->isa("URI") || $_[0]->isa("URI::WithBase") )
+    },
 );
 
-coerce( Uri,
+register_type_constraint($uri);
+
+coerce( _Uri,
     from Str                 => via { URI->new($_) },
     from "Path::Class::File" => via { URI::file->new($_) },
     from "Path::Class::Dir"  => via { URI::file->new($_) },
-    from ScalarRef           => via { URI::data->new($_) },
+    from ScalarRef           => via { my $u = URI->new("data:"); $u->data($$_); $u },
     from HashRef             => via { uri(%$_) },
-};
+);
 
 class_type FileUri, { class => "URI::file", parent => $uri };
 
@@ -45,11 +54,17 @@ coerce( FileUri,
     from "Path::Class::Dir"  => via { URI::file->new($_) },
 );
 
-subtype DataUri, as Uri, where { $_->scheme eq 'data' };
+subtype DataUri, as _Uri, where {
+    if ( my $scheme = $_->scheme ) {
+        return $scheme eq 'data';
+    } else {
+        return undef;
+    }
+};
 
 coerce( DataUri,
-    from Str       => via { URI::data->new($_) },
-    from ScalarRef => via { URI::data->new($_) },
+    from Str       => via { my $u = URI->new("data:"); $u->data($_);  $u },
+    from ScalarRef => via { my $u = URI->new("data:"); $u->data($$_); $u },
 );
 
 __PACKAGE__
